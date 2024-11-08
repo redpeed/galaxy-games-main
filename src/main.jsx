8b660d7774214c3b8413505c175e108a14517182
Leonardo Marazzi <leonardo@italiangamesfactory.com>
2024-11-08 13:20:38 +0100

    Posthog added

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // Only import BrowserRouter
 import App from './App.jsx';
 import './index.css';
+import posthog from 'posthog-js'
+
+posthog.init('phc_HPTKdXtOr1eHJV1V0SBUmWpGRAgz6rCkDeT55wCKOyK',
+    {
+        api_host: 'https://eu.i.posthog.com',
+        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+    }
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
