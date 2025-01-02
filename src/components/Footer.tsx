6030a4aeeb90cfaf487bc6c078cee35570783c1e
Leonardo Marazzi <leonardo@italiangamesfactory.com>
2025-01-02 15:30:31 +0100

    Removing from the navbar web3 and removing web3 also from the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,7 @@ import { FaTelegram, FaXTwitter } from 'react-icons/fa6';
 import { SiOpensea } from 'react-icons/si';
 import { Link } from 'react-router-dom';
 
-
 function Footer() {
-
   const [email, setEmail] = useState('');
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [lastSubmittedTime, setLastSubmittedTime] = useState(0);
@@ -18,16 +16,17 @@ function Footer() {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-  
+
     const now = Date.now();
     if (now - lastSubmittedTime < 5000) {
       alert('Please wait 5 seconds before submitting again.');
       return;
     }
     setLastSubmittedTime(now);
-  
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbzRL4gTqBNZFbLhuX8iyFoX_Hn945km4dhiiclurGuIckoAK15wtwUW12e_-VpCEx9R/exec';
-  
+
+    const scriptURL =
+      'https://script.google.com/macros/s/AKfycbzRL4gTqBNZFbLhuX8iyFoX_Hn945km4dhiiclurGuIckoAK15wtwUW12e_-VpCEx9R/exec';
+
     fetch(scriptURL, {
       method: 'POST',
       mode: 'no-cors',
@@ -39,7 +38,6 @@ function Footer() {
       .then(() => {
         setIsSubmitted(true);
         setEmail('');
-  
         // Optionally reset the button after 5 seconds
         setTimeout(() => {
           setIsSubmitted(false);
@@ -53,61 +51,61 @@ function Footer() {
 
   return (
     <div className="bg-primary text-slate-200 pt-10 pb-3 px-2">
-      <div data-aos="zoom-in" className="max-w-5xl  mx-auto">
+      <div data-aos="zoom-in" className="max-w-5xl mx-auto">
         <div className="flex flex-col md:flex-row items-center md:items-start justify-between mb-10 gap-5 md:gap-0">
           <div className="flex-[0.3]">
             <img src="/desktop/logo_v.png" className="w-40 mb-5" alt="" />
           </div>
+
+          {/* Navigation links in the footer */}
           <div className="hidden md:block md:flex-[0.3]">
-  <ul className="flex flex-col items-start justify-center uppercase text-sm 2xl:text-lg space-y-2 text-slate-400">
-    <li>
-      <a href="/#home" className="hover:text-gray-300">
-        Home
-      </a>
-    </li>
-    <li>
-      <a href="/#web3" className="hover:text-gray-300">
-        Web 2 meets web 3
-      </a>
-    </li>
-    <li>
-      <a href="/#games" className="hover:text-gray-300">
-        Games
-      </a>
-    </li>
-    <li>
-      <a href="/#token" className="hover:text-gray-300">
-        Token
-      </a>
-    </li>
-    <li>
-      <a href="/#roadmap" className="hover:text-gray-300">
-        Roadmap
-      </a>
-    </li>
-    <li className="relative pb-0">
-    <div className="flex flex-col items-center">
-  <a
-    href="https://galaxy-games-studio.gitbook.io/whitepaper"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    WHITEPAPER
-  </a>
-</div>
-    </li>
-    <li className="relative pb-0">
-      <div className="flex flex-col items-left">
-        <span className="line-through hover:text-gray-300 cursor-not-allowed">
-          STAKING PLATFORM
-        </span>
-        <span className="text-xs text-violet-500 uppercase font-bold whitespace-nowrap -mt-1">
-          COMING SOON
-        </span>
-      </div>
-    </li>
-  </ul>
-</div>
+            <ul className="flex flex-col items-start justify-center uppercase text-sm 2xl:text-lg space-y-2 text-slate-400">
+              <li>
+                <a href="/#home" className="hover:text-gray-300">
+                  Home
+                </a>
+              </li>
+              {/* Removed the "Web 2 meets web 3" link */}
+              <li>
+                <a href="/#games" className="hover:text-gray-300">
+                  Games
+                </a>
+              </li>
+              <li>
+                <a href="/#token" className="hover:text-gray-300">
+                  Token
+                </a>
+              </li>
+              <li>
+                <a href="/#roadmap" className="hover:text-gray-300">
+                  Roadmap
+                </a>
+              </li>
+              <li className="relative pb-0">
+                <div className="flex flex-col items-center">
+                  <a
+                    href="https://galaxy-games-studio.gitbook.io/whitepaper"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    WHITEPAPER
+                  </a>
+                </div>
+              </li>
+              <li className="relative pb-0">
+                <div className="flex flex-col items-left">
+                  <span className="line-through hover:text-gray-300 cursor-not-allowed">
+                    STAKING PLATFORM
+                  </span>
+                  <span className="text-xs text-violet-500 uppercase font-bold whitespace-nowrap -mt-1">
+                    COMING SOON
+                  </span>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          {/* Socials, newsletter, etc. */}
           <div className="flex-[0.4] text-right">
             <div className="hidden md:flex items-center justify-center md:justify-end gap-5 mb-8">
               <a
@@ -139,38 +137,40 @@ function Footer() {
                 <SiOpensea size={25} />
               </a>
             </div>
+
             <div className="mb-3 text-sm 2xl:text-lg tracking-[2.7px] leading-tight text-[#015cba] text-center md:text-right">
               <p>STAY UP TO DATE</p>
               <p>SUBSCRIBE TO OUR NEWSLETTER</p>
             </div>
-            
+
             <div className="w-80 md:ml-auto">
-            <form onSubmit={handleFormSubmit}>
-  <div className="flex items-center justify-center md:justify-end">
-    <input
-      type="email"
-      placeholder="E-mail address"
-      value={email}
-      onChange={handleInputChange}
-      required
-      className="pl-4 py-2 w-[70%] border text-sm focus:outline-none border-[#015cba] rounded-l-full bg-primary text-slate-400 placeholder-slate-300"
-    />
-<button
-  type="submit"
-  disabled={isSubmitted}
-  className={`h-10 ${
-    isSubmitted ? 'w-[60%]' : 'w-[30%]'
-  } text-white uppercase text-sm py-2 px-4 rounded-r-full duration-200 border ${
-    isSubmitted
-      ? 'bg-green-500 border-green-600 cursor-not-allowed'
-      : 'bg-gradient-to-b from-purple-600 to-indigo-600 hover:bg-gradient-to-t hover:to-purple-800 hover:from-indigo-800 border-purple-600'
-  }`}
->
-  {isSubmitted ? 'Email submitted!' : 'SIGN UP'}
-</button>
-  </div>
-</form>
+              <form onSubmit={handleFormSubmit}>
+                <div className="flex items-center justify-center md:justify-end">
+                  <input
+                    type="email"
+                    placeholder="E-mail address"
+                    value={email}
+                    onChange={handleInputChange}
+                    required
+                    className="pl-4 py-2 w-[70%] border text-sm focus:outline-none border-[#015cba] rounded-l-full bg-primary text-slate-400 placeholder-slate-300"
+                  />
+                  <button
+                    type="submit"
+                    disabled={isSubmitted}
+                    className={`h-10 ${
+                      isSubmitted ? 'w-[60%]' : 'w-[30%]'
+                    } text-white uppercase text-sm py-2 px-4 rounded-r-full duration-200 border ${
+                      isSubmitted
+                        ? 'bg-green-500 border-green-600 cursor-not-allowed'
+                        : 'bg-gradient-to-b from-purple-600 to-indigo-600 hover:bg-gradient-to-t hover:to-purple-800 hover:from-indigo-800 border-purple-600'
+                    }`}
+                  >
+                    {isSubmitted ? 'Email submitted!' : 'SIGN UP'}
+                  </button>
+                </div>
+              </form>
             </div>
+
             <div className="flex md:hidden items-center justify-center md:justify-end gap-5 mt-8">
               <a
                 href="https://discord.gg/p6zZDvgNUW "
@@ -203,29 +203,28 @@ function Footer() {
             </div>
           </div>
         </div>
-        <div className="border-t border-slate-700 text-slate-400 tracking-[2.7px] pt-3 pb-2 flex flex-col sm:flex-row items-center justify-between text-xs 2xl:text-base">
-  <p>© 2024 GALAXY GAMES FZCO. All Rights Reserved.</p>
-  <p>
-  <Link 
-        to="/privacypolicy" 
-        className="text-white-500 hover:text-violet-700 transition-colors duration-200 ml-2"
-      >
-        PRIVACY POLICY  
-      </Link>
-    {' '} -
-    <Link 
-        to="/termsandconditions" 
-        className="text-white-500 hover:text-violet-700 transition-colors duration-200 ml-2"
-      >
-        TERMS & CONDITIONS
-      </Link>
-  </p>
-</div>
 
+        <div className="border-t border-slate-700 text-slate-400 tracking-[2.7px] pt-3 pb-2 flex flex-col sm:flex-row items-center justify-between text-xs 2xl:text-base">
+          <p>© 2024 GALAXY GAMES FZCO. All Rights Reserved.</p>
+          <p>
+            <Link
+              to="/privacypolicy"
+              className="text-white-500 hover:text-violet-700 transition-colors duration-200 ml-2"
+            >
+              PRIVACY POLICY
+            </Link>{' '}
+            - 
+            <Link
+              to="/termsandconditions"
+              className="text-white-500 hover:text-violet-700 transition-colors duration-200 ml-2"
+            >
+              TERMS & CONDITIONS
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
 }
-
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import logoTwo from '../../public/desktop/logo-two.svg';
 
 const navLinks = [
   { id: 1, title: 'Home', href: '/#home' },
-  { id: 2, title: 'Web3', href: '/#web3' },
+  // Removed the Web3 link here
   { id: 3, title: 'Games', href: '/#games' },
   { id: 4, title: 'Token', href: '/#token' },
   { id: 5, title: 'Roadmap', href: '/#roadmap' },
@@ -37,93 +37,92 @@ function Navbar() {
   return (
     <>
       {/* Mobile Nav */}
-{/* Mobile Navbar */}
-<nav 
-className="flex lg:hidden fixed top-0 left-0 right-0 justify-between items-center px-4 py-3 text-white z-20"
-style={{ backgroundColor: '#01001e' }}>
-  <div className="flex items-center">
-    <img src={logoOne} alt="Logo" className="w-10 h-10" />
-    {/* If you want to include the second logo */}
-    {/* <img src={logoTwo} alt="Logo Two" className="w-20 ml-2" /> */}
-  </div>
+      <nav
+        className="flex lg:hidden fixed top-0 left-0 right-0 justify-between items-center px-4 py-3 text-white z-20"
+        style={{ backgroundColor: '#01001e' }}
+      >
+        <div className="flex items-center">
+          <img src={logoOne} alt="Logo" className="w-10 h-10" />
+          {/* If you want to include the second logo */}
+          {/* <img src={logoTwo} alt="Logo Two" className="w-20 ml-2" /> */}
+        </div>
 
-<div
-  onClick={() => setToggle((prev) => !prev)}
-  className="flex h-10 w-10 cursor-pointer items-center justify-center relative z-20"
->
-  {/* Hamburger Icon */}
-  <div className="space-y-1">
-    <span
-      className={`block w-8 bg-white transition-transform duration-300 ${
-        toggle ? 'transform rotate-45 translate-y-2' : ''
-      }`}
-      style={{ height: '2px' }}
-    ></span>
-    <span
-      className={`block w-8 bg-white transition-opacity duration-300 ${
-        toggle ? 'opacity-0' : ''
-      }`}
-      style={{ height: '2px' }}
-    ></span>
-    <span
-      className={`block w-8 bg-white transition-transform duration-300 ${
-        toggle ? 'transform -rotate-45 -translate-y-2' : ''
-      }`}
-      style={{ height: '2px' }}
-    ></span>
-  </div>
-</div>
-</nav>
+        <div
+          onClick={() => setToggle((prev) => !prev)}
+          className="flex h-10 w-10 cursor-pointer items-center justify-center relative z-20"
+        >
+          {/* Hamburger Icon */}
+          <div className="space-y-1">
+            <span
+              className={`block w-8 bg-white transition-transform duration-300 ${
+                toggle ? 'transform rotate-45 translate-y-2' : ''
+              }`}
+              style={{ height: '2px' }}
+            ></span>
+            <span
+              className={`block w-8 bg-white transition-opacity duration-300 ${
+                toggle ? 'opacity-0' : ''
+              }`}
+              style={{ height: '2px' }}
+            ></span>
+            <span
+              className={`block w-8 bg-white transition-transform duration-300 ${
+                toggle ? 'transform -rotate-45 -translate-y-2' : ''
+              }`}
+              style={{ height: '2px' }}
+            ></span>
+          </div>
+        </div>
+      </nav>
 
-{/* Overlay */}
-{toggle && (
-  <div
-    className="fixed inset-0 bg-black bg-opacity-50 z-10"
-    onClick={() => setToggle(false)}
-  ></div>
-)}
+      {/* Overlay */}
+      {toggle && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-10"
+          onClick={() => setToggle(false)}
+        ></div>
+      )}
 
-{/* Mobile Navigation Menu */}
-<div
-  className={`fixed top-0 left-0 h-full w-full bg-primary z-20 transform ${
-    toggle ? 'translate-y-0' : 'translate-x-full'
-  } transition-transform duration-300 flex flex-col`}
->
-  {/* Close Button */}
-  <div className="flex justify-end p-4">
-    <button
-      onClick={() => setToggle(false)}
-      className="text-white text-3xl focus:outline-none"
-    >
-      &times;
-    </button>
-  </div>
+      {/* Mobile Navigation Menu */}
+      <div
+        className={`fixed top-0 left-0 h-full w-full bg-primary z-20 transform ${
+          toggle ? 'translate-y-0' : 'translate-x-full'
+        } transition-transform duration-300 flex flex-col`}
+      >
+        {/* Close Button */}
+        <div className="flex justify-end p-4">
+          <button
+            onClick={() => setToggle(false)}
+            className="text-white text-3xl focus:outline-none"
+          >
+            &times;
+          </button>
+        </div>
 
-  {/* Logo at the Top Center */}
-  <div className="flex justify-center">
-    <img src={logoOne} alt="Logo" className="w-20 h-20" />
-  </div>
+        {/* Logo at the Top Center */}
+        <div className="flex justify-center">
+          <img src={logoOne} alt="Logo" className="w-20 h-20" />
+        </div>
 
-  {/* Menu Items */}
-  <div className="flex-grow flex items-center justify-center">
-    <ul className="flex flex-col items-center uppercase text-base space-y-6 text-slate-300 p-6 pt-0">
-      {navLinks.map((val) => (
-        <li key={val.id} onClick={() => setToggle(false)}>
-          <a href={val.href} className="hover:text-slate-100 text-4xl">
-            {val.title}
-          </a>
-        </li>
-      ))}
-    </ul>
-  </div>
-</div>
-      
+        {/* Menu Items */}
+        <div className="flex-grow flex items-center justify-center">
+          <ul className="flex flex-col items-center uppercase text-base space-y-6 text-slate-300 p-6 pt-0">
+            {navLinks.map((val) => (
+              <li key={val.id} onClick={() => setToggle(false)}>
+                <a href={val.href} className="hover:text-slate-100 text-4xl">
+                  {val.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
 
       {/* Desktop Nav */}
       <nav
         data-aos="fade-down"
         data-aos-delay="500"
-        className=" hidden absolute top-0 left-0 right-0 lg:flex justify-between items-center px-6 py-3 text-white z-20 bg-slate-900"
+        className="hidden absolute top-0 left-0 right-0 lg:flex justify-between items-center px-6 py-3 text-white z-20 bg-slate-900"
       >
         <div className="flex items-center space-x-4 gap-3">
           <div className="flex items-center justify-center gap-6">
@@ -155,7 +154,7 @@ style={{ backgroundColor: '#01001e' }}>
         </div>
 
         <a
-          href="https://t.me/HauntedSpaceMinigamebot/app?startapp=home "
+          href="https://t.me/HauntedSpaceMinigamebot/app?startapp=home"
           className="btn-primary px-7"
         >
           Play Now
